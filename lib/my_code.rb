f74d4a122cab523ebde82d5e5417091f8942162e
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 21:18:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,17 +35,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  sum = 0
-  i = 0
-  while i < source_array.length do
-    sum += source_array[i]
-    i += 1
-  end
-  if starting_point
-    sum += starting_point
-  end
-  sum
-end
+  
   
  
   

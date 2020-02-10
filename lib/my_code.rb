d7b424302d1bb246605ea71bc8bc2e83eeb1e626
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 20:54:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,3 +23,17 @@ def map_to_double(source_array)
   end 
   new_array
 end
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    new_array << source_array[i] * 2
+    i += 1
+  end 
+  new_array
+end
+
+
+
+
